a9a37bffacb912cd66db0ec580dc2b28b9d07d92
vishwajeet <singhvishwajeet1998@gmail.com>
2021-07-21 19:41:12 +0530

    video download fumctionality implemented

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 let videoPlayer = document.querySelector("video");
+let recordBtn = document.querySelector("#record");
 
-let promiseToCamera = navigator.mediaDevices.getUserMedia({ video: true });
+let mediaRecorder;
+let chunks = [];
+let isRecording = false;
+
+recordBtn.addEventListener("click", function () {
+  if (isRecording) {
+    mediaRecorder.stop();
+    isRecording = false;
+  } else {
+    mediaRecorder.start();
+    isRecording = true;
+  }
+});
+
+let promiseToCamera = navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true,
+});
 
 promiseToCamera
   .then(function (mediaStream) {
     videoPlayer.srcObject = mediaStream;
-    console.log(mediaStream);
+    mediaRecorder = new MediaRecorder(mediaStream);
+
+    mediaRecorder.addEventListener("dataavailable", function (e) {
+      chunks.push(e.data);
+    });
+
+    mediaRecorder.addEventListener("stop", function (e) {
+      let blob = new Blob(chunks, { type: "video/mp4" });
+      chunks = [];
+
+      // convert blob to link
+      let link = URL.createObjectURL(blob);
+
+      let a = document.createElement("a");
+      a.href = link;
+      a.download = "video.mp4";
+      a.click();
+    });
+
+    console.log(mediaRecorder);
   })
   .catch(function () {
     console.log("Permission denied");
