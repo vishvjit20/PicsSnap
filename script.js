23fe0d12a20f112ee14b7659df2bf43f509e4733
vishwajeet <singhvishwajeet1998@gmail.com>
2021-07-21 18:58:28 +0530

    initial navigator

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-navigator.mediaDevices
-  .getUserMedia({ video: true })
-  .then(function () {
-    console.log("Permission given");
+let videoPlayer = document.querySelector("video");
+
+let promiseToCamera = navigator.mediaDevices.getUserMedia({ video: true });
+
+promiseToCamera
+  .then(function (mediaStream) {
+    videoPlayer.srcObject = mediaStream;
+    console.log(mediaStream);
   })
   .catch(function () {
     console.log("Permission denied");
