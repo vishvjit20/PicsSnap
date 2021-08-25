1dcccbcd24614bd38999af88e29dee9d064f058c
vishwajeet <singhvishwajeet1998@gmail.com>
2021-08-25 15:40:49 +0530

    Implemented delete functionalities

--- dbScript.js
+++ dbScript.js
@@ -41,26 +41,43 @@ function viewMedia() {
       mediaCard.innerHTML = `<div class="actual-media"></div>
       <div class="media-buttons">
         <button class="media-download">Download</button>
-        <button class="media-delete">Delete</button>
+        <button data-mid="${cursor.value.mId}" class="media-delete">Delete</button>
       </div>`;
 
       let actualMediaDiv = mediaCard.querySelector(".actual-media");
+      let downloadBtn = mediaCard.querySelector(".media-download");
+      let deleteBtn = mediaCard.querySelector(".media-delete");
+      deleteBtn.addEventListener("click", function (e) {
+        let mId = Number(e.currentTarget.getAttribute("data-mid"));
+        deleteMedia(mId);
+        e.currentTarget.parentElement.parentElement.remove();
+      });
+
       let data = cursor.value.mediaData;
       let type = typeof data;
       if (type == "string") {
         // image
         let image = document.createElement("img");
         image.src = data;
+
+        downloadBtn.addEventListener("click", function () {
+          downloadMedia(data, "image");
+        });
+
         actualMediaDiv.append(image);
       } else if (type == "object") {
         // video
-        let url = URL.createObjectURL(data);
         let video = document.createElement("video");
-
+        let url = URL.createObjectURL(data);
         video.src = url;
         video.autoplay = true;
         video.loop = true;
         video.controls = true;
+
+        downloadBtn.addEventListener("click", function () {
+          downloadMedia(url, "video");
+        });
+
         actualMediaDiv.appendChild(video);
       }
 
@@ -68,4 +85,22 @@ function viewMedia() {
       cursor.continue();
     }
   });
+}
+
+function downloadMedia(url, type) {
+  let anchor = document.createElement("a");
+  anchor.href = url;
+  if (type == "image") {
+    anchor.download = "image.png";
+  } else if (type == "video") {
+    anchor.download = "video.mp4";
+  }
+  anchor.click();
+  anchor.remove();
+}
+
+function deleteMedia(mid) {
+  let tx = db.transaction("media", "readwrite");
+  let mediaObjectStore = tx.objectStore("media");
+  mediaObjectStore.delete(mid);
 }
